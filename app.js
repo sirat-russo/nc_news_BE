baa7175d8262f1031bb8641467e2e231a3fcdd70
mustaqim <sirat.russo@gmail.com>
2025-10-23 00:43:34 +0100

    Wire /api/topics route

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { getTopics } = require("./controllers/topics.controller");
+
+const app = express();
+
+app.get("/api/topics", getTopics);
+
+
+module.exports = app;
